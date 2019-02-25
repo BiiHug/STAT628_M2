5f8c982ce474e2c5f232b6e7cac3cb683a6f9cfe
BiiHug <34816685+BiiHug@users.noreply.github.com>
2019-02-25 00:08:58 -0600

    still struggling with cates...

--- code/real_yelp.py
+++ code/real_yelp.py
@@ -2,17 +2,21 @@ import json
 import pandas as pd
 import numpy as np
 import re
+from gensim.corpora.dictionary import Dictionary
 
+
+#这一个part是在处理商家数据
+##读入数据
 file3 = open('/Applications/Study/UWM/628/module2/data/business_train.json','r')
 busTrain = file3.readlines()
 file3.close()
-
 for i in range(len(busTrain)):
     busTrain[i]=json.loads(busTrain[i])
-
 dfBusTrain = pd.DataFrame.from_records(busTrain)
 dfBusTrain.head(5)
 
+
+##选出restaurant
 def checkRes(self):
     result = False
     try:
@@ -22,20 +26,27 @@ def checkRes(self):
     except AttributeError:
         result = False
     return result
-
 resIndex = np.where(dfBusTrain['categories'].apply(checkRes))
+###这里的idlist给出了restaurant的id
 resIdList = dfBusTrain['business_id'].iloc[resIndex]
-
 dfResTrain = dfBusTrain.iloc[resIndex]
 dfResTrain.drop(['is_open', 'postal_code'],axis=1,inplace=True)
 
-allCatList = []
+
+##处理categories，首先得到一个大的list：catListFrame
+dfResTrain['categories'].head(5)
+catListFrame = []
 def addCat(self):
     cat = re.sub('\s+|\)','',self.lower())
-    cat = re.sub('&|/|\(',',',cat).split(',')
-    allCatList.extend(cat)
+    cat = list(set(re.sub('&|/|\(',',',cat).split(',')) - set(['restaurants', 'restaurant', 'food']))
+    catListFrame.append(cat)
 dfResTrain['categories'].apply(addCat)
+###catListFrame可转化为dataframe
+dfCatFrame = pd.DataFrame.from_records(catListFrame)
+dfCatFrame.head(5)
+###catListFrame也可转化为series, 并进一步转化为词袋形式进行LDA
+restaurants_dictionary = Dictionary(pd.Series(catListFrame))
+restaurants_corpus = [restaurants_dictionary.doc2bow(text) for text in pd.Series(catListFrame)]
+restaurants_corpus[2]
+###待解决：是否只能用LDA？是否需要LDA？可以尝试一下LSA？
 
-catSeries = pd.Series(allCatList)
-catSeries.value_counts()
-catSeries.value_counts()[catSeries.value_counts()>500]
